831a7d02288ff51b5d8004427815bd15d4e042e1
Rahul Bisht <Rahul Bisht@HPENVY>
2023-02-12 23:09:43 -0800

    `Web application created

--- src/java/model/AccountService.java
+++ src/java/model/AccountService.java
@@ -5,30 +5,24 @@
  */
 package model;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 /**
  *
  * @author Rahul Bisht
  */
-public class AccountService extends HttpServlet {
-
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+public class AccountService 
+{
+    
+    public User login(String username, String password)
+    {
         
+    if((username.equals("abe") || username.equals("barb")) && password.equals("password"))
+    {
+            return new User(username, null);
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+        else 
+    {
+            return null;
     }
-
-
-}
+    }
+    
+ }
--- src/java/model/User.java
+++ src/java/model/User.java
@@ -5,31 +5,47 @@
  */
 package model;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.Serializable;
 
 /**
  *
  * @author Rahul Bisht
  */
-public class User extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
+public class User implements Serializable 
+{
+    
+    private String username;
+    private String password;
+    
+    
+    public User() //default cons
+    {
+    }
+    
+    User(String username, String password){
+        this.username = username;
+        this.password = password;
     }
 
-  
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-     
+    public String getUsername() 
+    {
+        return username;
     }
 
-  
+    public String getPassword() 
+    {
+        return password;
+    }
+
+    public void setUsername(String username) 
+    {
+        this.username = username;
+    }
+
+    public void setPassword(String password) 
+    {
+        this.password = password;
+    }
+    
+    
 }
